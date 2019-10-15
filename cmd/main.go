3ec602105981e204fe1c837034208b66c179227c
matsuyoshi <sfbgwm30@gmail.com>
2019-10-16 01:15:52 +0900

    Update.

--- cmd/main.go
+++ cmd/main.go
@@ -8,5 +8,6 @@ import (
 )
 
 func main() {
-	fmt.Println(pmjp.ToPmjp(time.Now()))
+	fmt.Println("1992/11/12 の日本の首相は ", pmjp.ToPmjp(1992, 11, 12))
+	fmt.Println(time.Now().Format("2006/01/02"), "の日本の首相は ", pmjp.ToPmjpFromTime(time.Now()))
 }
--- pmjp.go
+++ pmjp.go
@@ -4,7 +4,22 @@ import (
 	"time"
 )
 
-func ToPmjp(t time.Time) string {
+func ToPmjp(year, month, day int) string {
+	location, _ := time.LoadLocation("Asia/Tokyo")
+	if location == nil {
+		location = time.UTC
+	}
+	t := time.Date(year, time.Month(month), day, 0, 0, 0, 1, location)
+	for i := len(pms) - 1; i >= 0; i-- {
+		_t := time.Date(pms[i].StartYear, time.Month(pms[i].StartMonth), pms[i].StartDay, 0, 0, 0, 0, location)
+		if t.After(_t) {
+			return pms[i].Name
+		}
+	}
+	return ""
+}
+
+func ToPmjpFromTime(t time.Time) string {
 	location, _ := time.LoadLocation("Asia/Tokyo")
 	if location == nil {
 		location = time.UTC
